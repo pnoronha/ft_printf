6871b10481e57075273d2bd2bc3eced4cd29a241
pnoronha <pnoronha@student.42lisboa.com>
2021-07-08 04:13:24 +0100

    Initializing Header file

---
+++ libftprintf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pnoronha <pnoronha@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/07 17:07:24 by pnoronha          #+#    #+#             */
+/*   Updated: 2021/07/07 17:07:24 by pnoronha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include "libft/libft.h"
+# include <stdarg.h>
+
+int	ft_printf(const char *string, ...);
+
+#endif
