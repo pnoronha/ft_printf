951ab9b5eb3c13ec20d9ccd31a503f71a65d706a
pnoronha <pnoronha@student.42lisboa.com>
2021-08-05 20:08:24 +0100

    All functions added to header file

--- libftprintf.h
+++ libftprintf.h
@@ -16,6 +16,36 @@
 # include "libft/libft.h"
 # include <stdarg.h>
 
+typedef struct t_tab
+{
+	int			index;
+	int			length;
+	char		*imput;
+	va_list		*args;
+}				s_tab;
+
+typedef struct	t_flags
+{
+	int			dot;
+	int			star;
+	int			minus;
+	int			zero;
+	int			width;
+	int 		type;
+	int			count;
+
+}				s_flags;
+
 int	ft_printf(const char *string, ...);
+void	passThrough_string(s_tab *tab);
+void	conversion_select(s_tab *tab);
+void	conversion_character(s_tab *tab);
+void	conversion_string(s_tab *tab);
+void	conversion_decimal(s_tab *tab);
+void	conversion_pointer(s_tab *tab);
+void	conversion_unsigned(s_tab *tab);
+void	conversion_hexaDecimal(s_tab *tab);
+void	init_struct(s_tab *tab, char *string, va_list *args);
+s_flags	init_flags(void);
 
 #endif
