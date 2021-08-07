f72fd59ef2c2b0f2a4e9a79f3991ad3f6216777b
pnoronha <pnoronha@student.42lisboa.com>
2021-08-07 12:35:48 +0100

    printf working for subject's mandatory requirements

--- sources/ft_printf.c
+++ sources/ft_printf.c
@@ -6,18 +6,33 @@
 /*   By: pnoronha <pnoronha@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/08 02:38:11 by pnoronha          #+#    #+#             */
-/*   Updated: 2021/07/08 16:35:50 by pnoronha         ###   ########.fr       */
+/*   Updated: 2021/08/07 11:37:47 by pnoronha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libftprintf.h"
 
+void	init_struct(s_tab *tab, char *string, va_list *args)
+{
+	tab->args = args;
+	tab->imput = string;
+	tab->length = 0;
+	tab->index = 0;
+}
+
 int	ft_printf(const char *string, ...)
 {
 	va_list	args;
-	va_start(args, string);
+	s_tab	tab;
+	char	*string_save;
 
+	va_start(args, string);
+	string_save = ft_strdup(string);
+	init_struct(&tab, string_save, &args);
+
+	passThrough_string(&tab);
 
 	va_end(args);
-	return (0);
+	free(string_save);
+	return (tab.length);
 }
